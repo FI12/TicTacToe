6f4058af381b5ef24e2660b817ecf249d7894758
Medienfreak <medienfreak.de@gmail.com>
2015-09-08 04:25:02 +0200

    Start now.

--- TicTacToe/src/Welcome.java
+++ TicTacToe/src/Welcome.java
@@ -4,6 +4,8 @@ public class Welcome {
 	public static void main(String[] args) {
 
 		System.out.println("Willkommen beim TicTacToe-Projekt :-)");
+		System.out.println("Sei dabei!");
+		
 		
 		// TODO: all
 	}
