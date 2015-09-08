19f5f942f8533fa4359c8b0db0dffe3792b9a064
CRiemer <zong@gmx.de>
2015-09-08 04:36:38 +0200

    Studid text goes here.

--- TicTacToe/src/Welcome.java
+++ TicTacToe/src/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 	public static void main(String[] args) {
 
 		System.out.println("Willkommen beim TicTacToe-Projekt :-)");
-		System.out.println("Sei dabei!");
+		System.out.println("Sei mit dabei!");
 		
 		
 		// TODO: all
